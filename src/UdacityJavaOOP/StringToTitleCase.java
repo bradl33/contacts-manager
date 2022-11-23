26e72521295e94a3c94b35b1bf1302eae69de734
Bradley <39657175+bradl33@users.noreply.github.com>
2022-11-23 16:24:10 +0300

    Added reference resource to StringToTitleCase.java

--- src/UdacityJavaOOP/StringToTitleCase.java
+++ src/UdacityJavaOOP/StringToTitleCase.java
@@ -3,6 +3,7 @@ package UdacityJavaOOP;
 public class StringToTitleCase{
 
     //Iterates characters in original text and converts text to Title Case
+    //Reference: https://www.baeldung.com/java-string-title-case
     public static String convertToTitleCaseIteratingChars(String originalText){
         if (originalText == null || originalText.isEmpty()){
             return originalText;
